52f94b3f0d510d8872e5d5bf8dfd7639047d06d0
hao liang <lhwill@foxmail.com>
2019-12-25 08:11:35 +0800

    1

---
+++ CRM_MVC/Common/CustomErrorPagesMiddleware.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM_MVC.Common
+{
+    public class CustomErrorPagesMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public CustomErrorPagesMiddleware(
+            RequestDelegate next,
+            ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<CustomErrorPagesMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                //(1) 跟踪Trace = 0
+                //(2) 调试Debug = 1
+                //(3) 信息 Information = 2
+                //(4) 警告 Warning = 3
+                //(5) 错误 Error = 4
+                //(6) 严重 Critical = 5
+
+                _logger.LogError(0, ex, $"{DateTime.Now},执行{context.Request.Method}发生异常！");
+
+                if (context.Response.HasStarted)
+                {
+                    _logger.LogWarning("The response has already started, the error page middleware will not be executed.");
+                    throw;
+                }
+                try
+                {
+                    context.Response.Clear();
+                    context.Response.StatusCode = 500;
+                    return;
+                }
+                catch (Exception ex2)
+                {
+                    _logger.LogError(0, ex2, "An exception was thrown attempting to display the error page.");
+                }
+                throw;
+            }
+            finally
+            {
+                var statusCode = context.Response.StatusCode;
+                if (statusCode == 404 || statusCode == 500)
+                {
+                    await ErrorPage.ResponseAsync(context.Response, statusCode);
+                }
+            }
+        }
+    }
+}
---
+++ CRM_MVC/Common/ErrorPage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM_MVC.Common
+{
+    public static class ErrorPage
+    {
+
+        public static async Task ResponseAsync(HttpResponse response, int statusCode)
+        {
+            if (statusCode == 404)
+            {
+                await response.WriteAsync(Page404);
+            }
+            else if (statusCode == 500)
+            {
+                // await response.WriteAsync(Page404);
+                await response.WriteAsync(Page500);
+            }
+        }
+        private static string Page404 => $"<script>location.href='/404.html'</script>";
+
+        private static string Page500 => $"<script>location.href='/500.html'</script>";
+    }
+}
